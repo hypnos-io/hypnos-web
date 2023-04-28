6a1ec28afb68d8a90df1ecf9166ba5a0ba931d1a
Gabriel Lima <lima.gabrieldsantos@gmail.com>
2023-04-28 19:21:02 -0400

    feat: change message when workstation has no employee

--- src/components/WorkstationCard/index.tsx
+++ src/components/WorkstationCard/index.tsx
@@ -13,14 +13,16 @@ interface Props {
 export const WorkstationCard: React.FC<Props> = ({workstation, status}) => {
   const {employee} = workstation
 
-  const fullName = employee
+  const hasEmployee = !!employee
+
+  const fullName = hasEmployee
     ? `${employee.firstName} ${employee.lastName}`
-    : 'Operador'
+    : 'Sem operador'
 
   const registration = employee ? `#${employee.registration}` : ''
 
   return (
-    <li className={`card ${status}`}>
+    <li className={`card ${!hasEmployee ? 'none' : ''} ${status}`}>
       <img
         src="https://images.unsplash.com/photo-1592948078640-39656341be54?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
         alt="profile"
