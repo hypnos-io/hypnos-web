746fd29a27cdaa888b341dbb2f9096ab4a048c45
Gabriel Lima <lima.gabrieldsantos@gmail.com>
2023-05-18 19:17:02 -0400

    fix: fix styles

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
+import {useState} from 'react'
 import searchIcon from '../assets/img/Icon ionic-ios-search@2x.png'
-import './style.css';
+import './style.css'
 
-function SearchBar({ onSearch }) {
-  const [query, setQuery] = useState('');
+function SearchBar({onSearch}) {
+  const [query, setQuery] = useState('')
 
   const handleInputChange = (event) => {
-    setQuery(event.target.value);
-    onSearch(event.target.value);
-  };
+    setQuery(event.target.value)
+    onSearch(event.target.value)
+  }
 
   return (
-    <div className="search-bar">
+    <div className="generic-search-bar">
       <input
         type="text"
         placeholder="Buscar"
@@ -20,7 +20,7 @@ function SearchBar({ onSearch }) {
       />
       <img className="search-icon" src={searchIcon}></img>
     </div>
-  );
+  )
 }
 
-export default SearchBar;
+export default SearchBar
