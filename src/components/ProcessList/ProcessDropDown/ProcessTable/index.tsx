7ffb51e74e0a6d629ec745976bbe42c0dfdb1bef
Gabriel Lima <lima.gabrieldsantos@gmail.com>
2023-05-26 02:14:52 -0400

    fix: add delete UC

--- src/components/ProcessList/ProcessDropDown/ProcessTable/index.tsx
+++ src/components/ProcessList/ProcessDropDown/ProcessTable/index.tsx
@@ -1,78 +1,91 @@
-import {useState, useEffect} from 'react';
+import './style.css'
 
-import './style.css';
-
-import arrowsUpDownIcon from '../../../assets/img/ProcessList/Grupo 183@2x.png';
-import blackPencilIcon from '../../../assets/img/ProcessList/Icon open-pencil@2x.png';
-import deleteIcon from '../../../assets/img/ProcessList/Icon material-delete@2x.png';
-import addJobIcon from '../../../assets/img/ProcessList/Icon feather-plus-circle@2x.png';
-import { Job } from '../../../../entities/job';
-import { Process } from '../../../../entities/process';
+import {Job} from '../../../../entities/job'
+import {Process} from '../../../../entities/process'
+import {JobService} from '../../../../services/job_service'
+import {Delete} from '../../../../use_cases/job/Delete'
+import arrowsUpDownIcon from '../../../assets/img/ProcessList/Grupo 183@2x.png'
+import addJobIcon from '../../../assets/img/ProcessList/Icon feather-plus-circle@2x.png'
+import deleteIcon from '../../../assets/img/ProcessList/Icon material-delete@2x.png'
 
 interface ProcessTableProps {
-    process: Process,
-    jobs: Job[],
-    setCurrentPage: (page: number) => void,
-    setCurrentProcess: (process: Process) => void
+  process: Process
+  jobs: Job[]
+  setCurrentPage: (page: number) => void
+  setCurrentProcess: (process: Process) => void
 }
 
-function ProcessTable({ process, jobs, setCurrentPage, setCurrentProcess }: ProcessTableProps) {
+function ProcessTable({
+  process,
+  jobs,
+  setCurrentPage,
+  setCurrentProcess,
+}: ProcessTableProps) {
+  function formatTime(dateString: Date): string {
+    const date = new Date(dateString)
+    const hours = date.getHours().toString()
+    const minutes = date.getMinutes().toString().padStart(2, '0')
+    return `${hours}:${minutes}`
+  }
 
-    function formatTime(dateString: Date): string {
-        const date = new Date(dateString);
-        const hours = date.getHours().toString();
-        const minutes = date.getMinutes().toString().padStart(2, '0');
-        return `${hours}:${minutes}`;
-    }
+  async function handleDeleteJob(job: Job) {
+    if (!job._id) return
+    const deleteUC = new Delete(new JobService())
+    await deleteUC.execute(process, job)
+    window.location.reload()
+  }
 
-    function renderJob(job: Job, index: number) {
-        return (
-            <tr key={`${index}-${job._id}`} className="table__row">
-                <td><img src={arrowsUpDownIcon}/></td>
-                <td>{job.name}</td>
-                <td>{job.sector ? job.sector.value : ''}</td>
-                <td>{job.durationInHours}h</td>
-                <td>{formatTime(job.startAt)}</td>
-                <td>{formatTime(job.endAt)}</td>
-                <td>{job.employeeSize}</td>
-                <td><img src={blackPencilIcon}/></td>
-                <td><img src={deleteIcon}/></td>
-            </tr>
-        );
-    }
-
+  function renderJob(job: Job, index: number) {
     return (
-        <>
-            <table className="process__table">
+      <tr key={`${index}-${job._id}`} className="table__row">
+        <td>
+          <img src={arrowsUpDownIcon} />
+        </td>
+        <td>{job.name}</td>
+        <td>{job.sector ? job.sector.value : ''}</td>
+        <td>{job.durationInHours}h</td>
+        <td>{formatTime(job.startAt)}</td>
+        <td>{formatTime(job.endAt)}</td>
+        <td>{job.employeeSize}</td>
+        <td onClick={() => handleDeleteJob(job)}>
+          <img src={deleteIcon} />
+        </td>
+      </tr>
+    )
+  }
 
-                <tbody>
-                    <tr className="table__header">
-                        <th></th>
-                        <th>Tarefa</th>
-                        <th>Setor</th>
-                        <th>Duração</th>
-                        <th>Entrada</th>
-                        <th>Saída</th>
-                        <th>Nº de operários</th>
-                        <th></th>
-                        <th></th>
-                    </tr>
+  return (
+    <>
+      <table className="process__table">
+        <tbody>
+          <tr className="table__header">
+            <th></th>
+            <th>Tarefa</th>
+            <th>Setor</th>
+            <th>Duração</th>
+            <th>Entrada</th>
+            <th>Saída</th>
+            <th>Nº de operários</th>
+            <th></th>
+            <th></th>
+          </tr>
 
-                    {jobs.map(renderJob)}
-                    
-                </tbody>
-            
-            </table>
+          {jobs.map(renderJob)}
+        </tbody>
+      </table>
 
-            <div className="add__task__container" onClick={() => {
-                setCurrentProcess(process);
-                setCurrentPage(1);
-            }}>
-                <img src={addJobIcon}/>
-                <p>Adicionar Tarefa</p>
-            </div>
-        </>
-    );
+      <div
+        className="add__task__container"
+        onClick={() => {
+          setCurrentProcess(process)
+          setCurrentPage(1)
+        }}
+      >
+        <img src={addJobIcon} />
+        <p>Adicionar Tarefa</p>
+      </div>
+    </>
+  )
 }
 
-export default ProcessTable;
+export default ProcessTable
--- src/components/WorkstationSynchronization/index.tsx
+++ src/components/WorkstationSynchronization/index.tsx
@@ -1,19 +1,21 @@
-import {useEffect, useState} from 'react'
+import {useContext, useEffect, useState} from 'react'
 
 import './style.css'
 
-import { Process } from "../../entities/process";
-import JobHeader from "../JobForms/JobHeader";
-import WorkstationSynchronizationHeader from './WorkstationSynchronizationHeader';
-import EmployeeSynchronizationCard from './EmployeeSynchronizationCard';
-import { Employee } from '../../entities/employee';
-import { FetchAllEmployees } from '../../use_cases/employees/FetchAll';
-import { EmployeeService } from '../../services/employee_service';
-import { FetchAllBySector } from '../../use_cases/workstation/FetchAllBySector';
-import { WorkstationService } from '../../services/workstation_service';
-import { Workstation } from '../../entities/workstation';
-import { Sector } from '../../entities/sector';
-import { Update } from '../../use_cases/workstation/Update';
+import {useNavigate} from 'react-router-dom'
+import {JobContext} from '../../contexts/JobFlow'
+import {Employee} from '../../entities/employee'
+import {Process} from '../../entities/process'
+import {Sector} from '../../entities/sector'
+import {Workstation} from '../../entities/workstation'
+import {JobService} from '../../services/job_service'
+import {WorkstationService} from '../../services/workstation_service'
+import {Create} from '../../use_cases/job/Create'
+import {FetchAllBySector} from '../../use_cases/workstation/FetchAllBySector'
+import {Update} from '../../use_cases/workstation/Update'
+import JobHeader from '../JobForms/JobHeader'
+import EmployeeSynchronizationCard from './EmployeeSynchronizationCard'
+import WorkstationSynchronizationHeader from './WorkstationSynchronizationHeader'
 
 interface WorkstationSynchronizationProps {
   process: Process
@@ -22,110 +24,139 @@ interface WorkstationSynchronizationProps {
 }
 
 interface LinkedEmployee {
-    employee: Employee,
-    workstation: Workstation | null
+  employee: Employee
+  workstation: Workstation | null
 }
 
-function WorkstationSynchronization({ process, setCurrentPage, jobScreenStep }: WorkstationSynchronizationProps) {
-    const [employees, setEmployees] = useState<Employee[]>([]);
-    const [sector, setSector] = useState<Sector | undefined>();
-    const [workstations, setWorkstations] = useState<Workstation[]>([]);
-    const [linkedEmployees, setLinkedEmployees] = useState<LinkedEmployee[]>([]);
+function WorkstationSynchronization({
+  process,
+  setCurrentPage,
+  jobScreenStep,
+}: WorkstationSynchronizationProps) {
+  const navigate = useNavigate()
+  const {employees: employeesContext, job: jobContext} = useContext(JobContext)
+  const [employees] = useState<Employee[]>(employeesContext)
+  const [sector] = useState<Sector | undefined>(jobContext?.sector)
+  const [workstations, setWorkstations] = useState<Workstation[]>([])
+  const [linkedEmployees, setLinkedEmployees] = useState<LinkedEmployee[]>([])
 
-    async function fetchEmployees() {
-        const fetchUC = new FetchAllEmployees(new EmployeeService());
-        const employees = await fetchUC.execute();
-        setEmployees(employees);
+  async function fetchWorkstations() {
+    if (sector) {
+      if (sector._id) {
+        const fetchUC = new FetchAllBySector(new WorkstationService())
+        const allWorkstations = await fetchUC.execute(sector._id)
+        const allWorkstationsWithoutEmployee = allWorkstations.filter(
+          (workstation) => !workstation.employee
+        )
+        setWorkstations(allWorkstationsWithoutEmployee)
+      }
+    }
+  }
+
+  function linkEmployeeToWorkstation() {
+    const employeesToWorkstations: LinkedEmployee[] = []
+
+    for (let i = 0; i < employees.length; i++) {
+      employeesToWorkstations.push({
+        employee: employees[i],
+        workstation: null,
+      })
     }
 
-    async function fetchWorkstations() {
-        if (sector) {
-            if (sector._id) {
-                const fetchUC = new FetchAllBySector(new WorkstationService());
-                const allWorkstations = await fetchUC.execute(sector._id);
-                setWorkstations(allWorkstations);
-            }
-        }
+    if (workstations.length >= employees.length) {
+      for (let i = 0; i < employees.length; i++) {
+        employeesToWorkstations[i].workstation = workstations[i]
+      }
+    } else {
+      for (let i = 0; i < workstations.length; i++) {
+        employeesToWorkstations[i].workstation = workstations[i]
+      }
     }
 
-    function linkEmployeeToWorkstation() {
-        const employeesToWorkstations: LinkedEmployee[] = [];
+    setLinkedEmployees(employeesToWorkstations)
+  }
 
-        for (let i = 0; i < employees.length; i++) {
-            employeesToWorkstations.push({
-                employee: employees[i],
-                workstation: null
-            });
-        }
+  async function updateWorkstations() {
+    const updateUC = new Update(new WorkstationService())
 
-        if (workstations.length >= employees.length) {   
-            for (let i = 0; i < employees.length; i++) {
-                employeesToWorkstations[i].workstation = workstations[i];
-            }
-        } else {
-            for (let i = 0; i < workstations.length; i++) {
-                employeesToWorkstations[i].workstation = workstations[i];
-            }
-        }
-
-        setLinkedEmployees(employeesToWorkstations);
+    for (const linkedEmployee of linkedEmployees) {
+      if (
+        linkedEmployee.workstation &&
+        linkedEmployee.workstation._id &&
+        sector?._id
+      ) {
+        await updateUC.execute(linkedEmployee.workstation._id, sector._id, {
+          employeeId: linkedEmployee.employee._id,
+        })
+      }
     }
+  }
 
-    // async function updateWorkstations() {
-    //     const updateUC = new Update(new WorkstationService());
-        
-    //     linkedEmployees.forEach(linkedEmployee => {
-    //         if (linkedEmployee.workstation) {
-    //             await updateUC.execute(linkedEmployee.workstation._id, sector?._id, { employeeId: linkedEmployee.employee._id });
-    //         }
-    //     })
-    // }
+  async function createJob() {
+    if (!process || !process._id || !jobContext) return
 
-    function renderCards(linkedEmployee: LinkedEmployee, index: number) {
-        return (
-            <EmployeeSynchronizationCard key={`${index}-${linkedEmployee.employee._id}`} linkedEmployee={linkedEmployee}></EmployeeSynchronizationCard>
-        );
+    const createUC = new Create(new JobService())
+    const createdJob = await createUC.execute(process._id, jobContext)
+    return createdJob
+  }
+
+  async function handleSubmit() {
+    try {
+      await updateWorkstations()
+      await createJob()
+      window.location.reload()
+    } catch (error) {
+      console.error('Erro ao cadastrar nova atividade.')
+      console.error(error)
     }
+  }
 
-    useEffect(() => {
-        fetchEmployees();
-    }, []);
-
-    useEffect(() => {
-        fetchWorkstations();
-    }, []);
-
-    useEffect(() => {
-        if (employees.length > 0 && workstations.length > 0) {
-            linkEmployeeToWorkstation();
-        }
-    }, [employees, workstations]);
-
+  function renderCards(linkedEmployee: LinkedEmployee, index: number) {
     return (
-        <div className="workstation__synchronization__page__container">
-            <JobHeader
-                process={process}
-                setCurrentPage={setCurrentPage}
-                jobScreenStep={jobScreenStep}
-            ></JobHeader>
-            <div className="workstation__synchronization__container">
-                <WorkstationSynchronizationHeader></WorkstationSynchronizationHeader>
-
-                <div className="employees__synchronization__wrapper">
-                    {linkedEmployees.map(renderCards)}
-                </div>
-                <div className="buttons__container">
-                    <button
-                        onClick={() => setCurrentPage(2)}
-                        className="button secondary cancel__button"
-                    >
-                        Voltar
-                    </button>
-                    <button className="button continue__button">Prosseguir</button>
-                </div>
-            </div>
-        </div>
+      <EmployeeSynchronizationCard
+        key={`${index}-${linkedEmployee.employee._id}`}
+        linkedEmployee={linkedEmployee}
+      ></EmployeeSynchronizationCard>
     )
+  }
+
+  useEffect(() => {
+    fetchWorkstations()
+  }, [])
+
+  useEffect(() => {
+    if (employees.length > 0 && workstations.length > 0) {
+      linkEmployeeToWorkstation()
+    }
+  }, [employees, workstations])
+
+  return (
+    <div className="workstation__synchronization__page__container">
+      <JobHeader
+        process={process}
+        setCurrentPage={setCurrentPage}
+        jobScreenStep={jobScreenStep}
+      ></JobHeader>
+      <div className="workstation__synchronization__container">
+        <WorkstationSynchronizationHeader></WorkstationSynchronizationHeader>
+
+        <div className="employees__synchronization__wrapper">
+          {linkedEmployees.map(renderCards)}
+        </div>
+        <div className="buttons__container">
+          <button
+            onClick={() => setCurrentPage(2)}
+            className="button secondary cancel__button"
+          >
+            Voltar
+          </button>
+          <button onClick={handleSubmit} className="button continue__button">
+            Prosseguir
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default WorkstationSynchronization;
+export default WorkstationSynchronization
